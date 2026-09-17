09003a1ca3c84370289432dec97971ec6ce71862
oxstranger21 <gjmgopi21@gmail.com>
2026-09-17 19:07:31 +0530

    java script practice

---
+++ datatype.js
@@ -0,0 +1,10 @@
+a = 15;
+b = 2.6;
+c = "Gopi";
+d = true;
+e = 'y';
+console.log(a, typeof a); // number
+console.log(b, typeof b);
+console.log(c, typeof c);
+console.log(d, typeof d);
+console.log(e, typeof e);
