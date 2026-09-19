c992ff3fef51a2db5bd712882cfae3d27c45f223
oxstranger21 <gjmgopi21@gmail.com>
2026-09-19 16:23:21 +0530

    Array in javaScript

---
+++ array.js
@@ -0,0 +1,11 @@
+arr = [1,2,3,4,5];
+console.log(arr);
+arr.push(6); // add from last 
+console.log(arr);
+arr.pop(); // remove from last 
+console.log(arr);
+
+arr.unshift(0); // add from first
+console.log(arr);
+arr.shift(); // remove from first
+console.log(arr);
